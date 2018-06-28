5506a134966bf1db5a1885f73e8c80c2c1c9f453
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-06-29 07:27:05 +0800

    02-Add-Form: CopyFrom 01

---
+++ 06-Forms/02-Add-Form/mysite/polls/views.py
@@ -0,0 +1,46 @@
+import datetime
+from django.shortcuts import get_object_or_404, render
+
+# Create your views here.
+from django.http import HttpResponse, Http404
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render(request, 'current_datetime.html',
+                  {'current_date': now})
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    return render(request, 'hours_ahead.html', locals())
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
