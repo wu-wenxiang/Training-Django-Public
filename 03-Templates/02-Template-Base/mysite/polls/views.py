3ecec6c42d5013aa41ce9408fadbbbcbd42160b6
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-06-28 13:03:34 +0800

    02-Template-Base::add base template

--- 03-Templates/02-Template-Base/mysite/polls/views.py
+++ 03-Templates/02-Template-Base/mysite/polls/views.py
@@ -10,7 +10,8 @@ def index(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    return render(request, 'time.html', {'current_date': now})
+    return render(request, 'current_datetime.html',
+                  {'current_date': now})
 
 def hours_ahead(request, offset):
     try:
@@ -18,5 +19,4 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render(request, 'hours_ahead.html', locals())
