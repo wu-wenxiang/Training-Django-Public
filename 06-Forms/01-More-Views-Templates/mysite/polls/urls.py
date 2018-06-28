2f24fa6971e1292d2914a20675795f290fa5cb80
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-06-29 07:25:14 +0800

    01-More-Views-Templates: add more views

--- 06-Forms/01-More-Views-Templates/mysite/polls/urls.py
+++ 06-Forms/01-More-Views-Templates/mysite/polls/urls.py
@@ -2,8 +2,16 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^time/plus/(\d{1,2})', views.hours_ahead, name='hours_ahead'),
     url(r'^time/', views.current_datetime, name='current_datetime'),
-    url(r'', views.index, name='index'),
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
---
+++ 06-Forms/README.md
@@ -0,0 +1,6 @@
+## 更多Urls, Views和Templates
+- 为Views添加更多函数
+    - `/polls/`
+    - `/polls/5/`
+    - `/polls/5/results/`
+    - `/polls/5/vote/`
