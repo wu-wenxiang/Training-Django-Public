04707361fdf8a9e9dc1ea414c9e12a51352d3b46
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-06-28 10:07:41 +0800

    01-Add-Template::CopyFrom-0202-Hours-Ahead

---
+++ 03-Templates/01-Add-Template/mysite/polls/views.py
@@ -0,0 +1,23 @@
+import datetime
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, Http404
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
