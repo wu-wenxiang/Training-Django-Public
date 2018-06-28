a18b12f775b4588ebf6d3503120171911eb35010
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-06-28 08:32:54 +0800

    02-Hours-Ahead::Hours-Ahead & TimeZone

--- 02-Dynamic-Urls/02-Hours-Ahead/mysite/polls/urls.py
+++ 02-Dynamic-Urls/02-Hours-Ahead/mysite/polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^time/', views.current_datetime, name='current_datetime'),
-    url('^$', views.index, name='index'),
+    url(r'^time/plus/(\d{1,2})', views.hours_ahead, name='hours_ahead'),
+    url(r'^time/', views.current_datetime, name='current_datetime'),
+    url(r'', views.index, name='index'),
 ]
--- 02-Dynamic-Urls/02-Hours-Ahead/mysite/polls/views.py
+++ 02-Dynamic-Urls/02-Hours-Ahead/mysite/polls/views.py
@@ -2,7 +2,7 @@ import datetime
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 def index(request):
@@ -11,4 +11,13 @@ def index(request):
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
