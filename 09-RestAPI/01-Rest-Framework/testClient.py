82c1122fd336113aeb8edb90ff77309a3701acfe
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-08-31 23:25:27 +0800

    add post demo, append_slash

--- 09-RestAPI/01-Rest-Framework/testClient.py
+++ 09-RestAPI/01-Rest-Framework/testClient.py
@@ -1,14 +1,20 @@
 import requests
 url = 'http://localhost:8000/api/questions'
+url = url.rstrip('/')+'/'
+ 
 response = requests.get(url)
 print(response.json())
 print(response.text)
 print(response.status_code)
-# data ={
-#     "id": 2,
-#     "question_text": "What's new2?",
-#     "pub_date": "2018-06-29T06:30:35+08:00"
-# }
-# response = requests.post(url, data=data)
-# print(response.text)
-# print(response.status_code)
+
+import random
+i = random.randint(10000, 20000)
+data ={
+    "id": i,
+    "question_text": "What's new{}?".format(i),
+    "pub_date": "2018-06-29T06:30:35+08:00"
+}
+response = requests.post(url, data=data)
+print(response.text)
+print(response.status_code)
+print(response.json())
